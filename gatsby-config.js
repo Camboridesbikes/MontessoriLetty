ec2c8df7bf083cb661916b24382719b986402593
Cameron Hattendorf <hattendorfcameron@gmail.com>
2020-06-06 01:27:17 -0700

    edits in config

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,12 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-plugin-netlify-cms`,
+    {
+      resolve: `gatsby-plugin-netlify-cms`,
+      options: {
+        enableIdentityWidget: true,
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
