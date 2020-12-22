f173fdcc4fb57021fec61b08e3320a8a3f048689
Cameron Hattendorf <hattendorfcameron@gmail.com>
2020-12-22 00:01:52 -0800

    updated connect page syles

--- src/pages/Connect.jsx
+++ src/pages/Connect.jsx
@@ -35,27 +35,34 @@ return(
     style={{width: `100%`, backgroundSize: 'cover', display: 'flex', WebkitJustifyContent: 'center'}}
     fluid={data.Background.childImageSharp.fluid} objectPosition="50% 50%">
     <div className='tc pg'
-      style={{backgroundColor: `#00000070`, textAlign: `center`,  width: `61.8%`, minWidth: '800px', marginBottom: '5em'}}>
+      style={{backgroundColor: `#00000095`, textAlign: `center`,  width: `61.8%`, minWidth: '800px', marginBottom: '5em'}}>
       <div style={{padding: ``, color: `white`}}>
-        <h3>Connect With Us</h3>
-        <form name="contact" method="POST" data-netlify-true="true" netlify-honeypot="bot-field" >
+        <h1>Connect With Us</h1>
+        <form name="contact" method="POST" data-netlify-true="true" netlify-honeypot="bot-field"
+          style={{display: 'flex', maxWidth: '1000px', flexDirection: 'column', alignItems: 'center'}}>
           <p style={{display: `none`}}>
             <label>Don’t fill this out if you're human: <input name="bot-field" /></label>
           </p>
-          <p>
-            <label htmlFor="name">Name: <input type="text" name='name' /></label>
-          </p>
-          <p>
-            <label htmlFor="email">Email: <input type="email" name='email' /></label>
-          </p>
-          <h4>What Is Your Role?</h4>
-          <div style={{display: `flex`, justifyContent: `space-around`, flexWrap: `wrap`}}>
+
+          <div style={{margin: '1.5em 0'}}>
+            <p>
+              <label htmlFor="name">Name: <input type="text" name='name' /></label>
+            </p>
+            <p>
+              <label htmlFor="email">Email: <input type="email" name='email' /></label>
+            </p>
+          </div>
+
+          <h2>What Is Your Role?</h2>
+          <div
+            style={{display: `flex`, justifyContent: `space-around`, flexWrap: `wrap`, width: '100%', marginBottom: '1em'}}>
             <p><label htmlFor="admin"> <input type="checkbox" name="admin" id="" /> Administrator </label></p>
             <p><label htmlFor="educator"> <input type="checkbox" name="educator" id="" /> Educator </label></p>
             <p><label htmlFor="parent"> <input type="checkbox" name="parent" id="" /> Parent </label></p>
           </div>
-          <h4>What Are You Contacting Us About</h4>
-          <div style={{display: `flex`, justifyContent: `space-around`, flexWrap: `wrap`}}>
+          <h2>What Are You Contacting Us About</h2>
+          <div
+            style={{display: `flex`, justifyContent: `space-around`, flexWrap: `wrap`, maxWidth: '900px', padding: '0 100px'}}>
             <p><label htmlFor="mentoring"> <input type="checkbox" name="mentoring" id="" /> Mentoring/Coaching </label>
             </p>
             <p><label htmlFor="leadership"> <input type="checkbox" name="leadership" id="" /> School Leadership </label>
@@ -67,13 +74,16 @@ return(
             <p><label htmlFor="schoolPackage"> <input type="checkbox" name="schoolPackage" id="" /> School Package
               </label></p>
           </div>
-          <div style={{textAlign: 'left'}}>
-            <p>
-              <label htmlFor="more">
-                <h4>Tell us more:</h4>
-              </label>
-            </p>
-            <textarea name='more' rows="4" cols="60" /></div>
+          <div style={{maxWidth: '800px'}}>
+            <div style={{textAlign: 'left', margin: '1em'}}>
+              <p>
+                <label htmlFor="more">
+                  <h2>Tell us more:</h2>
+                </label>
+              </p>
+              <textarea name='more' rows="4" cols="60" /></div>
+          </div>
+          
             <button className="button" type="submit"><strong>Submit</strong></button>
           </form>          
           
@@ -96,13 +106,13 @@ return(
             <p>
               <label htmlFor="email">Email: <input type="email" name='email'/></label>
             </p> 
-            <h4>What Is Your Role?</h4> 
+            <h2>What Is Your Role?</h2> 
             <div style={{display: `flex`, justifyContent: `space-around`, flexWrap: `wrap`}}>
               <p><label htmlFor="admin"> <input type="checkbox" name="admin" id=""/> Administrator </label></p>
               <p><label htmlFor="educator"> <input type="checkbox" name="educator" id=""/> Educator </label></p>
               <p><label htmlFor="parent"> <input type="checkbox" name="parent" id=""/> Parent </label></p>
             </div>
-            <h4>What Are You Contacting Us About</h4> 
+            <h2>What Are You Contacting Us About</h2> 
             <div style={{display: `flex`, justifyContent: `space-around`, flexWrap: `wrap`}}>
               <p><label htmlFor="mentoring"> <input type="checkbox" name="mentoring" id=""/> Mentoring/Coaching </label></p>
               <p><label htmlFor="leadership"> <input type="checkbox" name="leadership" id=""/> School Leadership </label></p>
@@ -112,7 +122,7 @@ return(
             </div>
             <div style={{textAlign: 'left'}}> 
               <p >
-                <label htmlFor="more"><h4>Tell us more:</h4> </label>
+                <label htmlFor="more"><h2>Tell us more:</h2> </label>
               </p> 
               <textarea name='more' rows="4" cols="60"/></div>
             <button type="submit">Submit</button>
